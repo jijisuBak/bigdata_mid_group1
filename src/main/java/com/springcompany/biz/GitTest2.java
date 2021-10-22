2fb9c37d88e8041f098b845acafccad46293bf0c
user <user@DESKTOP-MF6GKVI>
2021-10-22 10:32:20 +0900

    5

--- src/main/java/com/springcompany/biz/GitTest2.java
+++ src/main/java/com/springcompany/biz/GitTest2.java
@@ -10,6 +10,10 @@ public class GitTest2 {
 	}
 	
 	public void gitTest4() {
-		System.out.println("깃허브 연동 테스트 4 학원");
+		System.out.println("깃허브 연동 테스트 4 in 학원");
+	}
+	
+	public void gitTest5() {
+		System.out.println("깃허브 연동 테스트 5 in 학원");
 	}
 }
