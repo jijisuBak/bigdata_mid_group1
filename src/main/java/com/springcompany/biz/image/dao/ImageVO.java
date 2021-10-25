211cf905a33313dad2793790853868cb569f3ed9
studyQuest <85128487+studyQuest@users.noreply.github.com>
2021-10-25 15:40:20 +0900

    15-40

--- src/main/java/com/springcompany/biz/image/dao/ImageVO.java
+++ src/main/java/com/springcompany/biz/image/dao/ImageVO.java
@@ -9,4 +9,35 @@ public class ImageVO {
 	private Set<String> tagList;
 	private Date takenPhotoDate;
 	private Date uploadDate;
+	
+	public int getImageKey() {
+		return imageKey;
+	}
+	public void setImageKey(int imageKey) {
+		this.imageKey = imageKey;
+	}
+	public String getMasterId() {
+		return masterId;
+	}
+	public void setMasterId(String masterId) {
+		this.masterId = masterId;
+	}
+	public Set<String> getTagList() {
+		return tagList;
+	}
+	public void setTagList(Set<String> tagList) {
+		this.tagList = tagList;
+	}
+	public Date getTakenPhotoDate() {
+		return takenPhotoDate;
+	}
+	public void setTakenPhotoDate(Date takenPhotoDate) {
+		this.takenPhotoDate = takenPhotoDate;
+	}
+	public Date getUploadDate() {
+		return uploadDate;
+	}
+	public void setUploadDate(Date uploadDate) {
+		this.uploadDate = uploadDate;
+	}
 }
